191d45d705b85c4e1938abffc171f25cfebc31e9
josh-delfin <123255126+josh-delfin@users.noreply.github.com>
2025-03-23 19:14:21 +0800

    add feedback

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const feedbackController = require('../controllers/feedbackController');
+const feedbackController = require('../Controllers/feedbackControllers');
 
 // Submit feedback
 router.post('/', feedbackController.submitFeedback);
